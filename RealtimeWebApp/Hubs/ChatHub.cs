d1136a9e3e57a6db672b8e37324bbd56c67aa192
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-03-18 22:27:06 +0700

    Strongly typed hubs

--- RealtimeWebApp/Hubs/ChatHub.cs
+++ RealtimeWebApp/Hubs/ChatHub.cs
@@ -2,21 +2,26 @@
 
 namespace RealtimeWebApp.Hubs
 {
-    public class ChatHub : Hub
+    public interface IChatClient
+    {
+        Task ReceiveMessage(string user, string message);
+    }
+
+    public class ChatHub : Hub<IChatClient>
     {
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.ReceiveMessage(user, message);
         }
 
         public Task SendMessageToCaller(string user, string message)
         {
-            return Clients.Caller.SendAsync("ReceiveMessage", user, message);
+            return Clients.Caller.ReceiveMessage(user, message);
         }
 
         public Task SendMessageToGroup(string user, string message)
         {
-            return Clients.Group("SignalR Users").SendAsync("ReceiveMessage", user, message);
+            return Clients.Group("SignalR Users").ReceiveMessage(user, message);
         }
     }
 }
