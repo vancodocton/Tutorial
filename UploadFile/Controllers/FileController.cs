6d7c598650bd7397a8c97271e29fb04d5439fdba
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-03-19 22:37:01 +0700

    Add actions to download and delete uploaded file

--- UploadFile/Controllers/FileController.cs
+++ UploadFile/Controllers/FileController.cs
@@ -31,5 +31,90 @@ namespace UploadFile.Controllers
                 .ToListAsync();
             return viewModel;
         }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UploadToFileSystem(List<IFormFile> files, string description)
+        {
+            foreach (var file in files)
+            {
+                var basePath = Path.Combine(Directory.GetCurrentDirectory() + "\\Files\\");
+
+                bool basePathExists = System.IO.Directory.Exists(basePath);
+                if (!basePathExists) Directory.CreateDirectory(basePath);
+
+                var fileName = Path.GetFileNameWithoutExtension(file.FileName);
+                var filePath = Path.Combine(basePath, file.FileName);
+                var extension = Path.GetExtension(file.FileName);
+
+                if (!System.IO.File.Exists(filePath))
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                    var fileModel = new FileOnFileSystem
+                    {
+                        CreatedOn = DateTime.UtcNow,
+                        FileType = file.ContentType,
+                        Extension = extension,
+                        Name = fileName,
+                        Description = description,
+                        FilePath = filePath,
+                        UploadedBy = "",
+                    };
+
+                    context.FilesOnFileSystem.Add(fileModel);
+
+                    await context.SaveChangesAsync();
+                }
+            }
+
+            TempData["Message"] = "File successfully uploaded to File System.";
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> DownloadFileFromFileSystem(int id)
+        {
+
+            var file = await context.FilesOnFileSystem
+                .Where(x => x.Id == id)
+                .FirstOrDefaultAsync();
+
+            if (file == null)
+                return null;
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(file.FilePath, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+
+            memory.Position = 0;
+
+            return File(memory, file.FileType, file.Name + file.Extension);
+        }
+
+        public async Task<IActionResult> DeleteFileFromFileSystem(int id)
+        {
+
+            var file = await context.FilesOnFileSystem
+                .Where(x => x.Id == id)
+                .FirstOrDefaultAsync();
+
+            if (file == null) 
+                return null;
+
+            if (System.IO.File.Exists(file.FilePath))
+            {
+                System.IO.File.Delete(file.FilePath);
+            }
+
+            context.FilesOnFileSystem.Remove(file);
+            context.SaveChanges();
+
+            TempData["Message"] = $"Removed {file.Name + file.Extension} successfully from File System.";
+            return RedirectToAction("Index");
+        }
     }
 }
